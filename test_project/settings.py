e5437a08bceca8be831f5ffcac03748794860dfd
miclon <jcnd@163.com>
2022-02-03 21:02:54 +0800

    add notification channel: chanify

--- test_project/settings.py
+++ test_project/settings.py
@@ -3,14 +3,18 @@ CHANNELS = {
     'PUSHDEER': {
         'TOKEN': ""
     },
+    'CHANIFY': {
+        'TOKEN': ""
+    },
 }
 
 TRIGGERS = {
-    'notify.channels.ding.Ding': 20,
-    'notify.channels.email.Email': 100,
-    'notify.channels.pushover.PushOver': 100,
-    'notify.channels.bark.Bark': 100,
-    'notify.channels.pushdeer.PushDeer': 100,
+    # 'notify.channels.ding.Ding': 20,
+    # 'notify.channels.email.Email': 100,
+    # 'notify.channels.pushover.PushOver': 100,
+    # 'notify.channels.bark.Bark': 100,
+    'notify.channels.chanify.Chanify': 100,
+    # 'notify.channels.pushdeer.PushDeer': 100,
     # 'notify.channels.wechat.WeChat': 100,
     'channels.custom.Custom': 10,
 }

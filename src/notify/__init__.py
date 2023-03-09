8d75289206c33dd175de4d078f214ed3d477b87e
miclon <jcnd@163.com>
2023-03-09 14:31:30 +0800

    🦄 refactor

--- src/notify/__init__.py
+++ src/notify/__init__.py
@@ -1,18 +1,3 @@
-"""
-
-notify = useNotify.wechat(
-    configs={...}
-)
-
-notify.send("")
-
-useNotify(
-    channels={},
-    triggers={}
-)
-
-"""
-
 from src.notify.notification import Notify
 
 useNotify = Notify
